0236d9a66d96087c349c7c9626160a26290634bb
Hackbright Student <no-reply@hackbrightacademy.com>
2019-03-26 16:40:54 -0700

    First commit

---
+++ easier_coding_challenges.py
@@ -0,0 +1,49 @@
+"""
+https://fellowship.hackbrightacademy.com/materials/challenges/_all.html#whiteboard-easier
+
+"Easier":
+- Add to Zero
+- Anagram of Palindrome
+- Binary Search
+- Concatenate Lists
+- Count List Recursively
+- Days in Month
+- Decimal to Binary
+- Decode a String
+- Find Lucky Numbers
+- Find the Range
+- FizzBuzz
+- Has More Vowels
+- Has Unique Characters
+- Is Number Prime?
+- Is Palindrome?
+- Largest Smaller Than
+- Lazy Lemmings
+- Leaping Lemur
+- Leet Speak
+- Longest Word
+- Max Number
+- Max of Three
+- Missing Number
+- Pangram
+- Pig Latin
+- Prime Number Generator
+- Print Digits Backwards
+- Print List Recursively
+- Recursive Index
+- Remove Duplicates
+- Remove Linked List Node
+- Replace Vowels
+- Reverse LinkedList
+- Reverse LinkedList In Place
+- Reverse a String Recursively
+- Show Even Numbers
+- Snake case to camel case
+- Sort Sorted Lists
+- Split a STring
+- Sum List
+- Sum List Recursively
+- Word Count
+- Word Lengths
+
+"""
