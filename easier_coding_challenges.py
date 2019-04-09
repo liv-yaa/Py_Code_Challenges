3c57c850f9ef8fc34bccf3ee168552908c19fa63
vagrant <vagrant@vagrant.vm>
2019-04-09 22:43:17 +0000

    numUniqueEmails

--- easier_coding_challenges.py
+++ easier_coding_challenges.py
@@ -379,12 +379,12 @@ def repeatedNTimes(A) -> int:
             return(k)
 
 
-def sortedSquares(A: List[int]) -> List[int]:
+def sortedSquares(A):
     
         return sorted(item ** 2 for item in A)
 
         
-def sortArrayByParity(A: List[int]) -> List[int]:
+def sortArrayByParity(A):
     
     out = [even for even in A if even % 2 == 0]
     out.extend([odd for odd in A if odd % 2 == 1])
@@ -395,6 +395,46 @@ def toLowerCase(str: str) -> str:
     
     return "".join([l.lower() for l in str])
 
+
+def numUniqueEmails(emails) -> int:
+        
+    """
+    Every email consists of a local name and a domain name, separated by the @ sign.
+
+    For example, in alice@leetcode.com, alice is the local name, and leetcode.com is the domain name.
+
+    Besides lowercase letters, these emails may contain '.'s or '+'s.
+
+    If you add periods ('.') between some characters in the local name part of an email address, mail sent there will be forwarded to the same address without dots in the local name.  For example, "alice.z@leetcode.com" and "alicez@leetcode.com" forward to the same email address.  (Note that this rule does not apply for domain names.)
+
+    If you add a plus ('+') in the local name, everything after the first plus sign will be ignored. This allows certain emails to be filtered, for example m.y+name@email.com will be forwarded to my@email.com.  (Again, this rule does not apply for domain names.)
+
+    It is possible to use both of these rules at the same time.
+
+    Given a list of emails, we send one email to each address in the list.  How many different addresses actually receive mails? 
+
+    Example 1:
+
+    Input: ["test.email+alex@leetcode.com","test.e.mail+bob.cathy@leetcode.com","testemail+david@lee.tcode.com"]
+    Output: 2
+    Explanation: "testemail@leetcode.com" and "testemail@lee.tcode.com" actually receive mails
+    """
+    uniques = set()
+    
+    for email in emails:
+        e = email.split("@")
+        local = e[0]
+        domain = e[1]
+                    
+        if '+' in local:
+            local = local[:local.index('+')]
+        
+        if '.' in local:
+            local = "".join(local.split("."))
+        
+        uniques.add(local + "@" + domain)
+    
+    return len(uniques)
 
 
 # Doctest Section:
