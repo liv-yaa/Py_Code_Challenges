d16ebdbef715d359af3e43a28b29b2df314fe9ee
vagrant <vagrant@vagrant.vm>
2019-04-10 16:21:12 +0000

    judgeCircle

--- easier_coding_challenges.py
+++ easier_coding_challenges.py
@@ -457,7 +457,33 @@ def addStrings(num1: str, num2: str) -> str:
     return str(summer)
 
 
+def flipAndInvertImage(A):
+    """
+    Given a binary matrix A, we want to flip the image horizontally, then invert it, and return the resulting image.
 
+    To flip an image horizontally means that each row of the image is reversed.  For example, flipping [1, 1, 0] horizontally results in [0, 1, 1].
+
+    To invert an image means that each 0 is replaced by 1, and each 1 is replaced by 0. For example, inverting [0, 1, 1] results in [1, 0, 0].
+    """
+    out = []
+    for sublist in A:
+        rev = sublist[::-1]
+        rev_out = []
+        
+        for i in range(len(rev)):
+            if rev[i] == 0:
+                rev_out.append(1)
+            elif rev[i] == 1:
+                rev_out.append(0)
+                
+        out.append(rev_out)
+        
+    return out
+        
+def judgeCircle(moves: str) -> bool:
+    
+    return (moves.count('D') == moves.count('U') and moves.count('L') == moves.count('R'))     
+            
 
 # Doctest Section:
 if __name__ == '__main__':
