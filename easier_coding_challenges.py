4a203b5f631e553b136d089e15539f77f3b9f0f4
vagrant <vagrant@vagrant.vm>
2019-04-10 22:25:18 +0000

    sortArrayByParityII

--- easier_coding_challenges.py
+++ easier_coding_challenges.py
@@ -569,6 +569,7 @@ def minDeletionSize(A) -> int:
 def sortArrayByParityII(A):
     
     """
+    https://leetcode.com/problems/sort-array-by-parity-ii/submissions/
     Given an array A of non-negative integers, half of the integers in A are odd, and half of the integers are even.
 
     Sort the array so that whenever A[i] is odd, i is odd; and whenever A[i] is even, i is even.
