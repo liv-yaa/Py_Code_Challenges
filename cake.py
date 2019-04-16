458b50f747d39b7f25c7426e2cf7d994e83e78fb
vagrant <vagrant@vagrant.vm>
2019-04-16 20:26:11 +0000

    Binary search fxn

--- cake.py
+++ cake.py
@@ -4,4 +4,53 @@ Interview Cake - probs
 """
 
 def binary_search(target, nums):
-    """See if target appears in nums"""
+    """See if target appears in nums, Recursively
+
+    https://www.interviewcake.com/question/python3/two-egg-problem
+
+    >>> binary_search(10, [1, 2, 3, 4, 10, 5, 6, 7, 8, 9])
+    True
+    >>> binary_search(10, [1, 2, 3, 4, 5, 6, 7, 8, 9])
+    False
+    
+
+    """
+    # Choose "walls" around the position of target
+    
+    floor_ix = -1 # -1 is to the left of the 0th index
+    ceil_ix = len(nums) # Last index
+
+    # If there is nothing between floor and ceil, the number must not be present
+    while floor_ix + 1 < ceil_ix:
+
+        # Find the index ~ halfway between flor and ceil
+        distance = ceil_ix - floor_ix
+        half_dist = distance // 2
+        guess_ix = floor_ix + half_dist
+
+        guess = nums[guess_ix]
+
+        if guess == target:
+            return True
+
+        if guess > target:
+            # Target is to the left, so move ceil to left
+            ceil_ix = guess_ix
+
+        else:
+            # Target is to the right, so move floor to right
+            floor_ix = guess_ix
+
+
+    return False
+
+
+
+# Doctest Section:
+if __name__ == '__main__':
+    import doctest
+
+    result = doctest.testmod()
+    if result.failed == 0:
+        print('ALL TESTS PASSED')
+
