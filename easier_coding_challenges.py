827c4f8c4091845b62756e02582a19487daa1a5f
vagrant <vagrant@vagrant.vm>
2019-04-16 23:35:35 +0000

    Add to zero fxn

--- easier_coding_challenges.py
+++ easier_coding_challenges.py
@@ -52,6 +52,32 @@ X repeatedNTimes
 
 
 """
+def add_to_zero(nums):
+    """ Given a list of ints, return true if any two nums in list sum to 0.
+    >>> add_to_zero([])
+    False
+
+    >>> add_to_zero([1])
+    False
+
+    >>> add_to_zero([1, 2, 3])
+    False
+
+    >>> add_to_zero([1, 2, 3, -2])
+    True
+    """
+    set_nums = set(nums) # Gives us faster lookup time in O(1) rather than O(n)
+
+    for n in nums:
+        if -n in set_nums:
+            return True
+
+    return False
+
+
+
+
+
 def show_evens(nums):
     """Given list of ints, return list of *indices* of even numbers in list.
 
@@ -830,7 +856,53 @@ class InterestingCodingNotes:
 
 
 
+    ## SET NOTATION PY3 ##
 
+        A | B 
+        A.union(B)
+        Returns a set which is the union of sets A and B.
+        
+        A |= B 
+        A.update(B)
+        Adds all elements of array B to the set A.
+        
+        A & B 
+        A.intersection(B)
+        Returns a set which is the intersection of sets A and B.
+        
+        A &= B 
+        A.intersection_update(B)
+        Leaves in the set A only items that belong to the set B.
+        
+        A - B 
+        A.difference(B)
+        Returns the set difference of A and B (the elements included in A, but not included in B).
+        
+        A -= B 
+        A.difference_update(B)
+        Removes all elements of B from the set A.
+        
+        A ^ B 
+        A.symmetric_difference(B)
+        Returns the symmetric difference of sets A and B (the elements belonging to either A or B, but not to both sets simultaneously).
+        
+        A ^= B 
+        A.symmetric_difference_update(B)
+        Writes in A the symmetric difference of sets A and B.
+        
+        A <= B 
+        A.issubset(B)
+        Returns true if A is a subset of B.
+        
+        A >= B 
+        A.issuperset(B)
+        Returns true if B is a subset of A.
+        
+        A < B
+        Equivalent to A <= B and A != B
+        
+        A > B
+        Equivalent to A >= B and A != B
 
 
 
