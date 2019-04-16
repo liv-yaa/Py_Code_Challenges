2291b3a24d5e404a9d22c2c89c67e268b5857e33
vagrant <vagrant@vagrant.vm>
2019-04-16 22:08:30 +0000

    Binary search fxn

--- cake.py
+++ cake.py
@@ -6,13 +6,16 @@ Interview Cake - probs
 def binary_search(target, nums):
     """See if target appears in nums, Recursively
 
+    Runtime is 
+
     https://www.interviewcake.com/question/python3/two-egg-problem
 
     >>> binary_search(10, [1, 2, 3, 4, 10, 5, 6, 7, 8, 9])
     True
     >>> binary_search(10, [1, 2, 3, 4, 5, 6, 7, 8, 9])
     False
-    
+
+
 
     """
     # Choose "walls" around the position of target
@@ -43,6 +46,47 @@ def binary_search(target, nums):
 
 
     return False
+
+
+
+def merge_sort(list_to_sort):
+    """
+    
+    """
+    # Base case: lists with fewer than 2 elements are sorted
+    if len(list_to_sort) < 2:
+        return list_to_sort
+
+    # Step 1: divide the list in half
+    # We use integer division, so we'll never get a "half index"
+    mid_index = len(list_to_sort) / 2
+    left  = list_to_sort[:mid_index]
+    right = list_to_sort[mid_index:]
+
+    # Step 2: sort each half
+    sorted_left  = merge_sort(left)
+    sorted_right = merge_sort(right)
+
+    # Step 3: merge the sorted halves
+    sorted_list = []
+    current_index_left = 0
+    current_index_right = 0
+
+    # sortedLeft's first element comes next
+    # if it's less than sortedRight's first
+    # element or if sortedRight is exhausted
+    while len(sorted_list) < len(left) + len(right):
+        if ((current_index_left < len(left)) and
+                (current_index_right == len(right) or
+                 sorted_left[current_index_left] < sorted_right[current_index_right])):
+            sorted_list.append(sorted_left[current_index_left])
+            current_index_left += 1
+        else:
+            sorted_list.append(sorted_right[current_index_right])
+            current_index_right += 1
+    return sorted_list
+
+
 
 
 
