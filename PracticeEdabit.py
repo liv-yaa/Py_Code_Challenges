878a1007e8c34984ee99a094e7b3d684e4b2610d
liv <olivia.f.smith@gmail.com>
2019-09-14 20:23:08 -0700

    more method

--- PracticeEdabit.py
+++ PracticeEdabit.py
@@ -54,7 +54,7 @@ def first_before_second(string, l1, l2):
 	return True
 		
 
-def convert_to_hex(txt):
+# def convert_to_hex(txt):
 	"""
 	>>> convert_to_hex("hello world")
 	'68 65 6c 6c 6f 20 77 6f 72 6c 64'
@@ -65,16 +65,16 @@ def convert_to_hex(txt):
 	"""
 	# return '4d 61 72 74 79 20 50 6f 70 70 69 6e 73 6f 6e'
 
-	out = ""
+	# out = ""
 
-	for char in txt:
-		# print(char)
+	# for char in txt:
+	# 	# print(char)
 
-		if char != ' ':
+	# 	if char != ' ':
 
-			out += str(ord(char)) + ' '
+	# 		out += str(ord(char)) + ' '
 
-	return out
+	# return out
 
 
 def matrixElementsSum(matrix):
@@ -108,6 +108,96 @@ def allLongestStrings(inputArray):
     out = [item for item in inputArray if len(item) == ml]
 
     return out
+
+
+def commonCharacterCount(s1, s2):
+    # "common" means the minimum count of characters, if they overlap
+    
+    common = set()
+    total = 0
+    
+    for l in s1:
+        if l in s2 and l not in common:
+            # get min
+            common.add(l)
+            total += min(s1.count(l), s2.count(l))
+
+    return total
+
+
+def isLucky(n):
+    # A ticket number is considered lucky if the sum of the first half of the digits is equal to the sum of the second half.
+    # Has even number of digits
+    
+    sum1 = sum([int(i) for i in str(n)[ : len(str(n)) // 2]])
+    sum2 = sum([int(i) for i in str(n)[len(str(n)) // 2 : ]])
+            
+    return sum1 == sum2
+
+
+def advanced_sort(lst):
+	""" Create a function that takes a list of numbers or strings and returns a 
+	list with the items from the original list stored into sublists. 
+	Items of the same value should be in the same sublist.
+	>>> advanced_sort([2, 1, 2, 1])
+	[[2, 2], [1, 1]]
+	>>> advanced_sort([5, 4, 5, 5, 4, 3])
+	[[5, 5, 5], [4, 4], [3]]
+	>>> advanced_sort(["b", "a", "b", "a", "c"])
+	[['b', 'b'], ['a', 'a'], ['c']]
+	
+	# Works:
+	outlist = []
+	for item in set(lst):
+		outlist.append([item] * (lst.count(item)))
+		
+		print(outlist)
+
+	return(outlist)
+
+
+	"""
+
+	outlist = []
+	passed = []
+
+	for item in lst:
+		if item not in passed:
+			outlist.append([item] * (lst.count(item)))
+			passed.append(item)
+
+	return(outlist)
+
+
+def is_prim_pyth_triple(lst):
+	""" 
+	Return if a^2 + b^2 = c^2 is true
+	AND
+	the greatest common prime factor between any two numbers is 1.
+
+	>>> is_prim_pyth_triple([4, 5, 3])
+	True
+	>>> is_prim_pyth_triple([7, 12, 13])
+	False
+	>>> is_prim_pyth_triple([39, 15, 36])
+	False
+	>>> is_prim_pyth_triple([77, 36, 85])
+	True
+
+	"""
+	l = sorted(lst)
+	a, b, c = l[0], l[1], l[2]
+
+	# Find GCP of all three numbers
+	a_factors = [i for i in range(1, a + 1) if a % i == 0]
+	b_factors = [i for i in range(1, b + 1) if b % i == 0]
+	c_factors = [i for i in range(1, c + 1) if c % i == 0]
+
+
+	common = [i for i in range(1, c + 1) if (i in a_factors) and (i in b_factors) and (i in c_factors)]
+
+	return (a**2 + b**2 == c**2) and (common == [1])
+
 
 
 if __name__ == "__main__":
