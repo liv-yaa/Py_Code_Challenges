7091265d14740a86f559c151e963850841dacb46
liv <olivia.f.smith@gmail.com>
2019-12-13 11:11:50 -0800

    fizzBuzz

--- leetcodepractice.py
+++ leetcodepractice.py
@@ -1074,8 +1074,16 @@ def singleNumber(self, nums):
 
 
 
-
-
+def fizzBuzz(m):
+    for n in range(1, m + 1):
+        if n % 15 == 0:
+            print('FizzBuzz')
+        elif n % 3 == 0:
+            print('Fizz')
+        elif n % 5 == 0:
+            print('Buzz')
+        else:
+            print(n)
 
 
 
