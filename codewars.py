ab4cd386fd70a787dd18b575b53e32efc8eba06f
liv <olivia.f.smith@gmail.com>
2019-12-21 02:21:28 +0100

    toJadenCase

--- codewars.py
+++ codewars.py
@@ -17,6 +17,30 @@ def reorder(arr, n):
 	return list(partition(arr, n))
 
 
+def solve(p):
+    nSum(p)
+    
+def nSum(num):
+    # If num is n-sum, it returns n 
+    snum = str(num)
+    print('num', num)
+    
+    for i in range(1, len(snum) + 1):
+        # Get chunks of length i, starting from the end
+        lst = []
+        print('len', i)
+        for j in range(len(snum) - i + 1):
+            print('chunk', snum[j:j + i])
+            lst.append(snum[j:j + i])
+        print('lst', lst)
+
+
 def toJadenCase(string):
     return ' '.join([(word[0].upper() + word[1:]) for word in string.split(' ')])        
-    
+
+
+for d in range(1, l + 1):
+        print('d', d)
+        # Get all chunks of length d or lower
+        chx = [ snum[i:i + d] for i in range(0, l, d) ]
+        print('chx', chx)
