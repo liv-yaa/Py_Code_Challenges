12d3f86a59794942c58a0f97b2538789d43b40d0
liv <olivia.f.smith@gmail.com>
2020-01-06 13:25:46 -0800

    aded test_slicing

--- pybitesJan2020.py
+++ pybitesJan2020.py
@@ -6,7 +6,6 @@
 # Bite 38. Using ElementTree to parse XML: *NOT DONE* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 import xml.etree.ElementTree as ET
-
 class XMLParser:
 	"""
 	Parses XML using `ElementTree`:
@@ -62,13 +61,20 @@ class XMLParser:
 	    r = self.get_tree()
 	    m = self.get_movies()
 	    print('r', r, 'm', m)
+# if __name__ == "__main__":
+	# STUCK - need to look up answers
+	# x = XMLParser()
+	# x.get_tree()
+	# x.get_movies()
+	# x.get_movie_longest_runtime()
+
+
 
 
 # Bite 130. Analyzing basic JSON Car Data: ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 from collections import Counter
 import requests
-
 class JSONParser:
 	""" Data analysis with basic car data """
 
@@ -107,32 +113,44 @@ class JSONParser:
 	    		s.add(str(m['model']))
 
 	    return s 
-
-
-
 if __name__ == "__main__":
 	# See testCars.py for full test suite
 	j = JSONParser()
-	print(j.get_models('Nissan', 2000))
+	# print(j.get_models('Nissan', 2000))
 
 	models = j.get_models('Volkswagen', 2008)
-	print(models, len(models))
+	# print(models, len(models))
+
+
+# Bite 1105. Slice and dice ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+from string import ascii_lowercase
+
+text = """
+One really nice feature of Python is polymorphism: using the same operation
+on different types of objects.
+Let's talk about an elegant feature: slicing.
+You can use this on a string as well as a list for example
+'pybites'[0:2] gives 'py'.
+ The first value is inclusive and the last one is exclusive so
+here we grab indexes 0 and 1, the letter p and y.
+  When you have a 0 index you can leave it out so can write this as 'pybites'[:2]
+but here is the kicker: you can use this on a list too!
+['pybites', 'teaches', 'you', 'Python'][-2:] would gives ['you', 'Python']
+and now you know about slicing from the end as well :)
+keep enjoying our bites!
+"""
+
+
+def slice_and_dice(text):
+    """Get a list of words from the passed in text.
+       See the Bite description for step by step instructions"""
+    results = []
+    print('test')
 
 
 
 
-
-
-
-
-
-
-# if __name__ == "__main__":
-	# STUCK - need to look up answers
-	# x = XMLParser()
-	# x.get_tree()
-	# x.get_movies()
-	# x.get_movie_longest_runtime()
 
 
 
