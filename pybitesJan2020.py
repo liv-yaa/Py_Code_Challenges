0752a3863c0d42b87d1d5ec66f6466780c2a4b50
liv <olivia.f.smith@gmail.com>
2020-01-08 19:56:07 -0800

    strip_vowels

--- pybitesJan2020.py
+++ pybitesJan2020.py
@@ -412,6 +412,64 @@ class SliceDice:
 
 		return results
 
+# [String Manip] Bite 106. Strip out vowels and count the number of replacements - NOT DONE ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+from typing import Tuple
+class ZenReplace:
+	text = """
+		The Zen of Python, by Tim Peters
+
+		Beautiful is better than ugly.
+		Explicit is better than implicit.
+		Simple is better than complex.
+		Complex is better than complicated.
+		Flat is better than nested.
+		Sparse is better than dense.
+		Readability counts.
+		Special cases aren't special enough to break the rules.
+		Although practicality beats purity.
+		Errors should never pass silently.
+		Unless explicitly silenced.
+		In the face of ambiguity, refuse the temptation to guess.
+		There should be one-- and preferably only one --obvious way to do it.
+		Although that way may not be obvious at first unless you're Dutch.
+		Now is better than never.
+		Although never is often better than *right* now.
+		If the implementation is hard to explain, it's a bad idea.
+		If the implementation is easy to explain, it may be a good idea.
+		Namespaces are one honking great idea -- let's do more of those!
+		"""
+	vowels = 'aeiouAEIOU'
+
+	def strip_vowels(self, text: str = text) -> Tuple[str, int]:
+		"""Replace all vowels in the input text string by a star
+		character (*).
+		Return a tuple of (replaced_text, number_of_vowels_found)
+
+		So if this function is called like:
+		strip_vowels('hello world')
+
+		... it would return:
+		('h*ll* w*rld', 3)
+
+		The str/int types in the function defintion above are part
+		of Python's new type hinting:
+		https://docs.python.org/3/library/typing.html"""
+		reps = 0
+		out = ''
+		for line in text.strip().split('\n'):
+			for v in self.vowels:
+				line = line.replace(v, '*')
+			# print(line)
+			out += line + '\n'
+
+
+		return (out, out.count('*'))
+
+
+if __name__ == '__main__':
+	z = ZenReplace()
+	print(z.strip_vowels())
+
 
 # [Datetime Module] Bite 16. PyBites date generator - *NOT DONE* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 from datetime import datetime, timedelta 
@@ -434,7 +492,6 @@ class DateGenerator:
 			days += 1
 			if days % 100 == 0 or days % 365 == 0:
 				yield self.PYBITES_BORN + timedelta(days = days)
-
 if __name__ == '__main__':
 	dg = DateGenerator()
 	# gen = dg.gen_special_pybites_dates()
