812656672639d247cbe9887db29520785da0d036
liv <olivia.f.smith@gmail.com>
2020-01-15 12:01:17 -0800

    stuck

--- hackerrankMedium.py
+++ hackerrankMedium.py
@@ -1,6 +1,6 @@
 # hackerrankMedium.py
 
-# import math
+import math
 # import os
 # import random
 # import re
@@ -34,30 +34,41 @@ def formingMagicSquare(s):
 	"""
 
 	# There are 8 magic squares - check all permutations of [1-9]
+	all_magic_perms = [[list(p[:3]), list(p[3:6]), list(p[6:])] for p in permutations(range(1, 10)) if is_magic([p[:3], p[3:6], p[6:]])]
 
+	# Iterate through all_magic_perms and get cost for s to be converted:
+	# for perm in all_magic_perms:
+	perm = all_magic_perms[0]
+	cost = 0
+	print('p', perm)
+	print('s', s)
 
-	# print(s, is_magic(s))
+	for i in range(len(s)):
+		for j in range(len(s)):
 
-	all_magic_perms = []
+			if s == perm:
+				print('success! cost :', cost)
+				return cost
 
-	for p in permutations(range(1, 10)):
-		lp = list(p)
-		mp = [lp[:3], lp[3:6], lp[6:]]
+			# print(s[i][j], perm[i][j])
 
-		if is_magic(mp):
-			print(mp)
+			cst = abs(s[i][j] - perm[i][j])
+			print('cst', cst)
 
-	return all_magic_perms
+			if cst != 0:
+				perm[i][j] = s[i][j]
+				cost += cst
+				print('cost updated, perm: ', perm)
 
-
+	return cost
 
 
 
 if __name__ == '__main__':
 	s1 = [[4, 9, 2], [3, 5, 7], [8, 1, 5]]
-	formingMagicSquare(s1)
+	print(formingMagicSquare(s1))
 
-	s2 = [[5, 3, 4], [1, 5, 8], [6, 4, 2]]
+	# s2 = [[5, 3, 4], [1, 5, 8], [6, 4, 2]]
 	# print(formingMagicSquare(s2))
 
 
