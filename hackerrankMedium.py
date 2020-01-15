f2d63b58d3d1448e896c857c499a80d71aaad83b
liv <olivia.f.smith@gmail.com>
2020-01-15 12:09:08 -0800

    formingMagicSquare done (looked up solution)

--- hackerrankMedium.py
+++ hackerrankMedium.py
@@ -7,10 +7,11 @@ import math
 # import sys
 from itertools import permutations
 
-def is_magic(mat):
+def is_magic(m):
 	""" Determines if a linear list representing a matrix `mat` is magic 
 		Rows, columns, and diagonals
 	"""
+	mat = [m[:3], m[3:6], m[6:]]
 	rs = set([sum(mat[i]) for i in range(3)])
 	cs = set([sum([mat[j][i] for j in range(3)]) for i in range(3)])
 	ds = set([sum([mat[0][0], mat[1][1], mat[2][2]]), sum([mat[0][2], mat[1][1], mat[2][0]])])
@@ -19,7 +20,7 @@ def is_magic(mat):
 
 	return len(rs) == 1 and len(cs) == 1 and len(ds) == 1
 
-def formingMagicSquare(s):
+def formingMagicSquare(arr):
 	""" Def of MagicSquare:
 		An  `n x n ` matrix of distinct positive integers from  to  where the sum of any row, column, 
 		or diagonal of length  is always equal to the same number: the magic constant.
@@ -34,42 +35,24 @@ def formingMagicSquare(s):
 	"""
 
 	# There are 8 magic squares - check all permutations of [1-9]
-	all_magic_perms = [[list(p[:3]), list(p[3:6]), list(p[6:])] for p in permutations(range(1, 10)) if is_magic([p[:3], p[3:6], p[6:]])]
+	magic = [list(p) for p in permutations(range(1, 10)) if is_magic(p)]
 
-	# Iterate through all_magic_perms and get cost for s to be converted:
-	# for perm in all_magic_perms:
-	perm = all_magic_perms[0]
-	cost = 0
-	print('p', perm)
-	print('s', s)
-
-	for i in range(len(s)):
-		for j in range(len(s)):
-
-			if s == perm:
-				print('success! cost :', cost)
-				return cost
-
-			# print(s[i][j], perm[i][j])
-
-			cst = abs(s[i][j] - perm[i][j])
-			print('cst', cst)
-
-			if cst != 0:
-				perm[i][j] = s[i][j]
-				cost += cst
-				print('cost updated, perm: ', perm)
-
-	return cost
+	mini = 2**64
+	for brr in magic:
+		diff = 0
+		for i, j in zip(arr, brr):
+			diff += abs(i - j)
+		mini = min(diff, mini)
+	return mini
 
 
 
 if __name__ == '__main__':
-	s1 = [[4, 9, 2], [3, 5, 7], [8, 1, 5]]
-	print(formingMagicSquare(s1))
+	s1 = [4, 9, 2, 3, 5, 7, 8, 1, 5]
+	print(formingMagicSquare(s1)) # 1
 
-	# s2 = [[5, 3, 4], [1, 5, 8], [6, 4, 2]]
-	# print(formingMagicSquare(s2))
+	s2 = [4, 8, 2, 4, 5, 7, 6, 1, 6]
+	print(formingMagicSquare(s2))
 
 
 
