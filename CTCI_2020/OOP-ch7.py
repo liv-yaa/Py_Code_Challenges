8c744ffe570f9839d326fdf443aed4a5674d96e1
liv <olivia.f.smith@gmail.com>
2020-01-18 11:43:36 -0800

    DeckOfCards Blackjack

--- CTCI_2020/OOP-ch7.py
+++ CTCI_2020/OOP-ch7.py
@@ -45,26 +45,23 @@ Good Design Patterns - APPROACH:
 
 """
 
+import random
+
 # 7.1
 class Card:
-	""" Card """
+	""" Singleton class for a Card """
 
 	def __init__(self, value, suit):
 		""" Has attributes value and suit, when we instantiate a new card. """
-		
 		self.value = value
 		self.suit = suit
 
 	def __repr__(self):
 		""" String representation of Card """
-
 		return f'{self.suit} of {self.value}'
 
 class DeckOfCards:
-
-	""" Deck of Cards: Design the data structures for a generic deck of cards. Explain how you would
-	subclass the data structures to implement blackjack.
-
+	""" Singleton class for a Deck of Cards: Design the data structures for a generic deck of cards
 	>>> doc = DeckOfCards()
 	[1 of Spades, 2 of Spades, 3 of Spades, 4 of Spades, 5 of Spades, 6 of Spades, 7 of Spades, 8 of Spades, 9 of Spades, 10 of Spades, 11 of Spades, 12 of Spades, 13 of Spades, 1 of Clubs, 2 of Clubs, 3 of Clubs, 4 of Clubs, 5 of Clubs, 6 of Clubs, 7 of Clubs, 8 of Clubs, 9 of Clubs, 10 of Clubs, 11 of Clubs, 12 of Clubs, 13 of Clubs, 1 of Diamonds, 2 of Diamonds, 3 of Diamonds, 4 of Diamonds, 5 of Diamonds, 6 of Diamonds, 7 of Diamonds, 8 of Diamonds, 9 of Diamonds, 10 of Diamonds, 11 of Diamonds, 12 of Diamonds, 13 of Diamonds, 1 of Hearts, 2 of Hearts, 3 of Hearts, 4 of Hearts, 5 of Hearts, 6 of Hearts, 7 of Hearts, 8 of Hearts, 9 of Hearts, 10 of Hearts, 11 of Hearts, 12 of Hearts, 13 of Hearts]
 
@@ -81,9 +78,46 @@ class DeckOfCards:
 			print(c)
 
 	def build(self):
+		""" Builds up the deck """
 		for s in ['Spades', 'Clubs', 'Diamonds', 'Hearts']:
 			for v in range(1, 14):
 				self.deck.append(Card(s, v))
+
+	def shuffle(self):
+		""" Shuffles the cards randomly """
+		for i in range(len(self.deck)):
+			o = random.randint(i)
+			self.deck[i], self.deck[o] = self.deck[o], self.deck[i]
+
+	def draw_card(self):		
+		""" Pops off the top card """
+		return self.deck.pop()
+
+
+class Blackjack(DeckOfCards):
+	""" For a game of Blackjack, subclass the data structure of Deck of Cards to implement blackjack. 
+	copied https://github.com/w-hat/ctci-solutions/blob/master/ch-07-object-oriented-design/01-deck-of-cards.py
+	>>> bj = Blackjack()
+	[1 of Spades, 2 of Spades, 3 of Spades, 4 of Spades, 5 of Spades, 6 of Spades, 7 of Spades, 8 of Spades, 9 of Spades, 10 of Spades, 11 of Spades, 12 of Spades, 13 of Spades, 1 of Clubs, 2 of Clubs, 3 of Clubs, 4 of Clubs, 5 of Clubs, 6 of Clubs, 7 of Clubs, 8 of Clubs, 9 of Clubs, 10 of Clubs, 11 of Clubs, 12 of Clubs, 13 of Clubs, 1 of Diamonds, 2 of Diamonds, 3 of Diamonds, 4 of Diamonds, 5 of Diamonds, 6 of Diamonds, 7 of Diamonds, 8 of Diamonds, 9 of Diamonds, 10 of Diamonds, 11 of Diamonds, 12 of Diamonds, 13 of Diamonds, 1 of Hearts, 2 of Hearts, 3 of Hearts, 4 of Hearts, 5 of Hearts, 6 of Hearts, 7 of Hearts, 8 of Hearts, 9 of Hearts, 10 of Hearts, 11 of Hearts, 12 of Hearts, 13 of Hearts]
+	>>> bj.give_value()
+	"""
+	trans = {''}
+	def give_value(self):
+		""" Gives cards a respective value for this game """
+		v, aces = 0, 0
+		for card in self.deck:
+			print(card.value)
+			v += min(card.value, 10) # Doesnt work cuz we have a dict instead of vals
+			aces += 1
+			print(v, aces)
+
+		while v <= 11:
+			if aces:
+				v += 10
+				aces -= 1
+		return v
+
+
 
 
 
