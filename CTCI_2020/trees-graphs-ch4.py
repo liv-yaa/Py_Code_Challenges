1ca492a47790406d55f90e8d3647b7e5f48d6bd5
liv <olivia.f.smith@gmail.com>
2020-01-22 18:19:15 -0800

    update Tree methods

--- CTCI_2020/trees-graphs-ch4.py
+++ CTCI_2020/trees-graphs-ch4.py
@@ -41,6 +41,8 @@ Binary Search Tree
 	- All leaf nodes at same level
 	- If so, a perfect tree has 2^k - 1 nodes
 
+"Min-Heap"
+	- 
 
 """
 
@@ -48,89 +50,201 @@ class Node:
 	""" A general node for trees or graphs
 		>>> n3 = Node('n3')
 		>>> print(n3)
-
+		Leaf n3
 		>>> n2 = Node('n2', [n3]), 
 		>>> print(n2)
-
-		>>>
-		# Node n2[]
-		# >>> n1 = Node('n1', children=[n2, n3])
-		# >>> print(n1)
-		# Node n1[]
+		(Node n2 [Leaf n3],)
+		>>> n1 = Node('n1', children=[n2, Node('n4')])
+		>>> print(n1)
+		Node n1 [(Node n2 [Leaf n3],), Leaf n4]
 	"""
 
 	def __init__(self, data, children=[]):
 		self.data = data
-		self.children = children # left, right, ...
+		self.children = children
 
 	def __repr__(self):
 		# return self.children
 		if self.children == []:
-			return 'Leaf ' + self.data
+			return 'Leaf ' + str(self.data)
 		else:
-			return 'Node ' + self.data + ', children: ' + str(self.children)
+			return 'Node ' + str(self.data) + ' ' + str(self.children)
 
+	def find(self, data):
+		""" Return node object w/ this data """
 
-class BinaryNode(Node):
-	""" A Binary node for trees or graphs
-		# >>> bn = BinaryNode('bnode')
-		# >>> print(bn)
-		# Node bnode[]
-		# >>> bn.insert('test')
-		# >>> print(bn)
-	"""
+		# Start a list to keep track of nodes to visit.
+		to_visit = [self]
 
-	# def insert(self, data):
-	# 	# Compare the new value with the parent node
-	# 	if self.data:
+		while to_visit:
+			curr = to_visit.pop()
 
-	# 		# Try the left side
-	# 		if self.data < self.data:
+			if curr.data == data:
+				return curr
 
-	# 			# If left branch is empty, add new node to left
-	# 			if self.left == None:
-	# 				self.left = Node(data)
-
-
-		
-
-
-
-
-
-
+			# Else, add all children to 'to visit' list
+			to_visit.extend(curr.children)
 
 # She said these make Things more complicated
-
 class Tree:
 	""" Class representing a Tree
-		tree_root = Node('tree_root', [...])
-		t = Tree(tree_root)
+		>>> tree_root = Node('tree_root', [...])
+		>>> t = Tree(tree_root)
+		>>> print(t)
+		>>> t.find_in_tree(1)
+
 	"""
 
 	def __init__(self, root):
 		self.root = root
 
-class BinaryTree:
-	""" Class representing a Binary Tree
-		tree_root = Node('tree_root', [...])
-		t = Tree(tree_root)
+	def __repr__(self):
+		"""Reader-friendly representation."""
+		return "<Tree root={root}>".format(root=self.root)
+
+	def find_in_tree(self, data):
+		"""Return node object with this data.
+
+		Start at root.
+		Use the method from root to find the data
+		Return None if not found.
+		"""
+		return self.root.find(data)
+
+	def list_nodes_recursive(self, node):
+		print(node.data)
+		for child in node.children:
+			list_nodes_recursive(child)
+
+
+
+
+
+
+
+
+
+class BinaryNode():
+	""" A Binary node for trees or graphs
+
+		# Create root node:
+		>>> bn = BinaryNode(0)
+		>>> print(bn.data, bn.right, bn.left)
+		0 None None
+
+		# Add a new data - creates a new node & decides if it should go right or left:
+		>>> bl = BinaryNode(-1)
+		>>> bn.insert(-1)
+		>>> bn.PrintTree()
+		0
+
 	"""
+	def __init__(self, data):
+		self.left = None
+		self.right = None
+		self.data = data
 
-	def __init__(self, root):
-		self.root = root
+
+	def insert(self, data):
+		# *Recursively * Compare the value ' data ' of a new node with the 
+			# parent node ' self.data ' 
+			# and decides where to add it to the tree
+
+		# print('inserting', data, self.data)
+		if self.data != None: 
+			# print(self.data)
+
+			# Try the left side		
+			if data < self.data:
+
+				# If left branch is empty, add new node to left
+				if self.left == None:
+					
+					self.left = BinaryNode(data)
+					# print(self.left.data)
+				else:
+					self.left.insert(data) # Recursive call
+
+			elif data > self.data:
+				if self.right == None:
+					self.right = BinaryNode(data)
+				else:
+					self.right.insert(data)
+		else:
+			self.data = data
 
 
-	def in_order_traversal(self, node):
-		pass
+	def PrintTree(self):
+		if self.left:
+			self.left.PrintTree()
+			print(self.data)
+		if self.right:
+			self.right.PrintTree()
+
+"""
+Traversals:
+	- In order traversal
+			Visit the left branch, then current, then right
+	- Pre-order traversal
+			Visits the current node before its child nodes
+	- Post-order traversal
+			Visits the current node after its child nodes
+"""
+def in_order_traversal(treeNode):
+	""" Takes a TreeNode and visits the current nodes before its 
+		child nodes
+	"""
+	if treeNode != None:
+		in_order_traversal(node.left)
+		visit(node)
+		in_order_traversal(node.right)
+
+def pre_order_traversal(treeNode):
+	""" Takes a TreeNode and visits the current nodes before its 
+		child nodes
+	"""
+	if treeNode != None:
+		visit(node)
+		pre_order_traversal(node.left)
+		pre_order_traversal(node.right)
+
+
+
+def post_order_traversal(treeNode):
+	""" Takes a TreeNode and visits the current nodes after its 
+		child nodes
+
+		The root will always be the last node visited.
+	"""
+	if treeNode != None:
+		
+		pre_order_traversal(node.left)
+		post_order_traversal(node.right)
+		visit(node)
+
+
+
+
+
 
 
 
 if __name__ == "__main__":
-	import doctest
-	doctest.testmod()
+	# import doctest
+	# doctest.testmod()
 
+	# Make a filesystem
+	resume = Node("resume.txt", [])
+	recipes = Node("recipes.txt", [])
+	jane = Node("jane/", [resume, recipes])
+	server = Node("server.py", [])
+	jessica = Node("jessica/", [server])
+	users = Node("Users/", [jane, jessica])
+	root = Node("/", [users])
 
+	tree = Tree(root)
+	print("server.py = ", tree.find_in_tree("server.py")) # Will find
+	print("style.css = ", tree.find_in_tree("style.css")) # will not find
 
 
 
