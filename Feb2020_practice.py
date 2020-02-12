25206db91c3a3bd76d1136e0a10beaae4c1f9850
liv <olivia.f.smith@gmail.com>
2020-02-12 15:57:02 -0800

    swap_case

--- Feb2020_practice.py
+++ Feb2020_practice.py
@@ -12,4 +12,24 @@ import textwrap
 def textWrap():
 	S = raw_input()
 	w = input()
-	print textwrap.fill(S,w)
+	print(textwrap.fill(S,w))
+
+
+def swap_case(s):
+    out = ''
+    for c in s:
+        if c.isupper():
+            out += c.lower()
+        elif c.islower():
+            out += c.upper()
+        else:
+            out += c
+
+    return out
+
+if __name__ == '__main__':
+    s = 'HackerRank.com presents "Pythonist 2".'
+    result = swap_case(s)
+    print(result)
+
+
