6772dca8ace8e8e530472eb2fcdca7529e013781
liv <olivia.f.smith@gmail.com>
2020-02-11 16:13:27 -0800

    textWrap

--- Feb2020_practice.py
+++ Feb2020_practice.py
@@ -1,1 +1,15 @@
 # Feb2020_practice.py
+
+def matchingStrings(strings, queries):
+	""" """
+    results = [None] * len(queries)
+    for i, q in enumerate(queries):
+        results[i] = strings.count(q)
+    return results
+
+
+import textwrap
+def textWrap():
+	S = raw_input()
+	w = input()
+	print textwrap.fill(S,w)
