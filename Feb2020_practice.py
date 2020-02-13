176e6ed21ca8fe7cef1dc18859a70dae1765d480
liv <olivia.f.smith@gmail.com>
2020-02-12 16:13:11 -0800

    i cannot focus

--- Feb2020_practice.py
+++ Feb2020_practice.py
@@ -7,13 +7,10 @@ def matchingStrings(strings, queries):
         results[i] = strings.count(q)
     return results
 
-
 import textwrap
-def textWrap():
-	S = raw_input()
-	w = input()
-	print(textwrap.fill(S,w))
 
+def wrap(s, w):
+    return textwrap.fill(s,w)
 
 def swap_case(s):
     out = ''
@@ -27,9 +24,35 @@ def swap_case(s):
 
     return out
 
+# if __name__ == '__main__':
+#     s = 'HackerRank.com presents "Pythonist 2".'
+#     result = swap_case(s)
+#     print(result)
+
+
+def text_align(thickness):
+	# thickness = int(input()) #This must be an odd number
+	c = 'H'
+
+	#Top Cone
+	for i in range(thickness):
+	    print((c*i).rjust(thickness-1)+c+(c*i).ljust(thickness-1))
+
+	#Top Pillars
+	for i in range(thickness+1):
+	    print((c*thickness).center(thickness*2)+(c*thickness).center(thickness*6))
+
+	#Middle Belt
+	for i in range((thickness+1)//2):
+	    print((c*thickness*5).center(thickness*6))    
+
+	#Bottom Pillars
+	for i in range(thickness+1):
+	    print((c*thickness).center(thickness*2)+(c*thickness).center(thickness*6))    
+
+	#Bottom Cone
+	for i in range(thickness):
+	    print(((c*(thickness-i-1)).rjust(thickness)+c+(c*(thickness-i-1)).ljust(thickness)).rjust(thickness*6))
+
 if __name__ == '__main__':
-    s = 'HackerRank.com presents "Pythonist 2".'
-    result = swap_case(s)
-    print(result)
-
-
+    text_align(9)
