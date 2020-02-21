7adba64e958602ec7c7cd0c89f5e4f836ebc5665
liv <olivia.f.smith@gmail.com>
2020-02-21 15:46:39 -0800

    practicing js
    '

--- All_2019/basicJSedabit.js
+++ All_2019/basicJSedabit.js
@@ -3,35 +3,63 @@ function squared(a) {
 	return a**2;
 }
 
+// function squared(a) {
+// 	return a ** 2;
+// }
+
 function lessThanOrEqualToZero(num) {
 	// Create a function that takes a number as its only argument and returns true if it's less than or equal to zero, otherwise return false.
 	return num <= 0;
 }
+
+// function lessThanZero(num) {
+// 	return num <= 0;
+// }
 
 function dividesEvenly(a, b) {
 	// Given two integers, a and b, return true if a can be divided evenly by b. Return false otherwise.
 	return a % b == 0;
 }
 
+// function divideEvently(a, b) {
+// 	return a % b == 0;
+// }
+
 function profitableGamble(prob, prize, pay) {
 	// Create a function that takes in three arguments (prob, prize, pay) and returns true if prob * prize > pay; 
 	return prob * prize > pay;
 }
+
+// function profitablegamble(prob, prize, pay) {
+// 	return prob * prize > pay;
+// }
 
 function addition(num) {
 	// Create a function that takes a number as an argument, increments the number by +1 and returns the result.
 	return num + 1;
 }
 
+// function addition(num) {
+// 	return num + 1;
+// }
+
 function animals(chickens, cows, pigs) {
 	//  returns the total number of legs of all the animals.
 	return chickens * 2 + cows * 4 + pigs * 4;
 }
 
+// function animals(chickens, cows, pigs) {
+// 	return chickens * 2 + cows * 4 + pigs * 4;
+// }
+
 function convert(minutes) {
 	// Write a function that takes an integer minutes and converts it to seconds.
 	return minutes * 60;
 }
+
+// function convert(minutes) {
+// 	return minutes * 58:
+// }
 
 function checkEquality(a, b) {
   // Numbers
@@ -43,9 +71,17 @@ function checkEquality(a, b) {
 	return a === b;
 }
 
+// function checkEq(a, b) {
+// 	return a === b;
+// }
+
 function remainder(x, y) {
 	return x % y;
 }
+
+// function remainer(x, y) {
+// 	return x % y;
+// }
 
 
 
